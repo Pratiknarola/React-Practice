e1fad1b369ed1a3208db095fde18d560d0e3f07a
Pratik Narola <pratik.narola@juspay.in>
2021-05-23 18:37:59 +0530

    array map done

--- src/index.js
+++ src/index.js
@@ -5,18 +5,40 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css'
 
+const books = [
+    {
+        link:"https://images-eu.ssl-images-amazon.com/images/I/61r-WomIz0L._AC_UL200_SR200,200_.jpg",
+        title: 'One Arranged Murder',
+        author: 'Chetan Bhagat'
+    },
+    {
+        link:'https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg',
+        title: 'The Alchemist',
+        author:'Paulo Coelho'
+    },
+    {
+        link: 'https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg',
+        title: 'The Blue Umbrella',
+        author: 'Ruskin Bond'
+    }
+]
+
 
 function App() {
     return <section className="booklist"> 
-        <Book link="https://images-eu.ssl-images-amazon.com/images/I/61r-WomIz0L._AC_UL200_SR200,200_.jpg" title='One Arranged Murder' author='Chetan Bhagat' />
-        <Book link='https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg' title='The Alchemist' author='Paulo Coelho' />
-        <Book link='https://images-eu.ssl-images-amazon.com/images/I/71JZ0neAP1L._AC_UL200_SR200,200_.jpg' title='The Blue Umbrella' author='Ruskin Bond' />
+        {
+            books.map((book) => {
+                return <Book data={book}></Book>
+            })
+        }
+
         </section>
 
 }
 
 
 function Book(props) {
+    props = props.data;
     return <article className="book">
         <img src={props.link} alt="" />
         <h1>{props.title}</h1>
